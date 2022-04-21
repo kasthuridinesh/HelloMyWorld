ba70cfd6e0cba0021c978d053341b24802dada97
kasthuridinesh <kasthuridinesh153@gmail.com>
2022-04-21 23:47:25 +0530

    commit

---
+++ HelloMyWorld/src/Kathurigit/HelloWorld.java
@@ -0,0 +1,13 @@
+package Kathurigit;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s = "kasthuri";
+		System.out.println(s);
+System.out.println("HelloWorld");
+	}
+
+}
